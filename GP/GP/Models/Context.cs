9cd4ce3d61b47e1b53a6b0e9245f867f37e7b788
uma Prithi <umaprithi@outlook.com>
2020-02-11 12:36:51 +0100

    Product tbl,Context class

---
+++ GP/GP/Models/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GP.Models
+{
+    public class Context:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+          => options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=GroupProject;Trusted_Connection=True;");
+    }
+}
